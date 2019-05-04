14f58b168e4fba60035ea7b5af246ac457202c6f
guilledelmo <33062373+guilledelmo@users.noreply.github.com>
2019-05-04 11:39:53 +0200

    Added all cryptos and grid

--- main.py
+++ main.py
@@ -13,22 +13,21 @@ def get_prices():
     data = json.loads(response.text)
     return data
 
-
+def show_labels():
+    for i in range(5):
+        tkinter.Label(main_win, text=data["data"][i]["id"], font=("Helvetica, 12")).grid(row=i+1, column=0)
+        tkinter.Label(main_win, text=data["data"][i]["name"], font=("Helvetica, 12")).grid(row=i+1, column=1)
+        tkinter.Label(main_win, text="%.2f" % data["data"][i]["quote"]["USD"]["price"], font=("Helvetica, 12")).grid(row=i+1, column=2)
+        tkinter.Label(main_win, text=str(data["data"][i]["quote"]["USD"]["percent_change_24h"]) + "%", font=("Helvetica, 12")).grid(row=i+1, column=3)
+    win_timestamp = tkinter.Label(main_win, text=data["status"]["timestamp"], font=("Helvetica", 8))
+    win_timestamp.grid(row=6, column=2, columnspan=2)
 data = get_prices()
 
 main_win = tkinter.Tk()
 main_win.title("Crypto prices")
 title_label = tkinter.Label(main_win, text="Crypto top 5", font=("Helvetica", 20))
-title_label.pack()
-first_label_id = tkinter.Label(main_win, text=data["data"][0]["id"], font=("Helvetica", 12))
-first_label_id.pack()
-first_label_name = tkinter.Label(main_win, text=data["data"][0]["name"], font=("Helvetica", 12))
-first_label_name.pack()
-first_label_price = tkinter.Label(main_win, text=data["data"][0]["quote"]["USD"]["price"], font=("Helvetica", 12))
-first_label_price.pack()
-first_label_change = tkinter.Label(main_win, text=str(data["data"][0]["quote"]["USD"]["percent_change_24h"]) + "%", font=("Helvetica", 12))
-first_label_change.pack()
-win_timestamp = tkinter.Label(main_win, text=data["status"]["timestamp"], font=("Helvetica", 8))
-win_timestamp.pack()
+title_label.grid(row=0, column=0, columnspan=4)
+show_labels()
+refresh_button = tkinter.Button(main_win, text="Refresh", command=show_labels).grid(row=6, column=0, columnspan=2)
 main_win.mainloop()
 # TODO: Refresh button that refreshes the window so that the labels need to call the function again to get the data, which should then be updated
