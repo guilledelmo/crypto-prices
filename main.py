2d4c7913294c04b6f335bf4406afacf0c1e3a98b
Guillermo <guillermo.delmolino@gmail.com>
2019-05-05 13:16:57 +0200

    Refresh button works now

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import tkinter
 import json
 
 def get_prices():
+    global data
     url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
     parameters = {'start':'1', 'limit':'5', 'convert':'USD'}
     headers = {'Accepts': 'application/json', 'X-CMC_PRO_API_KEY': 'KEY'}
@@ -21,13 +22,18 @@ def show_labels():
         tkinter.Label(main_win, text=str(data["data"][i]["quote"]["USD"]["percent_change_24h"]) + "%", font=("Helvetica, 12")).grid(row=i+1, column=3)
     win_timestamp = tkinter.Label(main_win, text=data["status"]["timestamp"], font=("Helvetica", 8))
     win_timestamp.grid(row=6, column=2, columnspan=2)
-data = get_prices()
+
+def update_data():
+    # Fetches the new data from API and refreshes labels
+    get_prices()
+    show_labels()
+
+get_prices()
 
 main_win = tkinter.Tk()
 main_win.title("Crypto prices")
 title_label = tkinter.Label(main_win, text="Crypto top 5", font=("Helvetica", 20))
 title_label.grid(row=0, column=0, columnspan=4)
 show_labels()
-refresh_button = tkinter.Button(main_win, text="Refresh", command=show_labels).grid(row=6, column=0, columnspan=2)
+refresh_button = tkinter.Button(main_win, text="Refresh", command=update_data).grid(row=6, column=0, columnspan=2)
 main_win.mainloop()
-# TODO: Refresh button that refreshes the window so that the labels need to call the function again to get the data, which should then be updated
